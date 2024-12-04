aaed0b9e0eb304e02d64acbac974860590c3d57a
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-12-04 22:52:46 +0100

    kid version

--- src/app/pages/kid-game-page/kid-game-page.component.ts
+++ src/app/pages/kid-game-page/kid-game-page.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './kid-game-page.component.html',
-  styleUrl: './kid-game-page.component.scss'
+  styleUrl: './kid-game-page.component.scss',
 })
 export class KidGamePageComponent {
-
+  isSick: boolean = false;
+  isHealthy: boolean = false;
+  isBasic: boolean = true;
+  setHamsterSick() {
+    this.isSick = true;
+    this.isBasic = false;
+    this.isHealthy = false;
+  }
+  setHamsterHealthy() {
+    this.isHealthy = true;
+    this.isBasic = false;
+    this.isSick = false;
+  }
 }
